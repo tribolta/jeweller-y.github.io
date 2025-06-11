58f316c80937e830f1ff1982ad4e6005e49667c3
tribolta <predurdom@yandex.ru>
2025-06-11 16:48:56 +0300

    Delete css directory

--- css/fonts/js/icon.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-/* Подготавливаем иконку меню */
-$('#nav-wrap').prepend('<div id="menu-icon">&#9776;</div>');
-    /* Включаем навигацию */
-    $("#menu-icon").on("click", function(){
-    $("#nav").slideToggle();
-        $(this).toggleClass("active");
-    });
-    });
